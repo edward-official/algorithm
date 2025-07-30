6c4ac4657a5eff33814cef91bbf4919ce6c476b6
edwardlee <openmoresome@gmail.com>
2025-07-30 21:31:12 +0900

    chapter 20 version 3 (3/8)

--- bj/src/Main.java
+++ bj/src/Main.java
@@ -11,9 +11,10 @@ public class Main {
     static class P15649_3 {
         private static int n, m;
         private static int[] array;
+        private static boolean[] isUsed;
 
         private static void recursive(int index) {
-            if(index>m) {
+            if(index > m) {
                 builder.append(array[1]);
                 for(int traverse=2; traverse<=m; traverse++) {
                     builder.append(" ").append(array[traverse]);
@@ -21,9 +22,13 @@ public class Main {
                 builder.append("\n");
                 return;
             }
+
             for(int element=1; element<=n; element++) {
+                if(isUsed[element]) continue;
                 array[index] = element;
-                recursive(index+1);
+                isUsed[element] = true;
+                recursive(index + 1);
+                isUsed[element] = false;
             }
         }
         public static void execute() throws IOException {
@@ -31,6 +36,7 @@ public class Main {
             n = Integer.parseInt(tokenizer.nextToken());
             m = Integer.parseInt(tokenizer.nextToken());
             array = new int[m+1];
+            isUsed = new boolean[n+1];
             recursive(1);
             out.write(builder.toString());
             out.flush();
@@ -66,10 +72,39 @@ public class Main {
             out.flush();
         }
     }
+    static class P15651_3 {
+        private static int n, m;
+        private static int[] array;
+
+        private static void recursive(int index) {
+            if(index > m) {
+                builder.append(array[1]);
+                for(int traverse=2; traverse<=m; traverse++) {
+                    builder.append(" ").append(array[traverse]);
+                }
+                builder.append("\n");
+                return;
+            }
+
+            for(int element=1; element<=n; element++) {
+                array[index] = element;
+                recursive(index+1);
+            }
+        }
+        public static void execute() throws IOException {
+            tokenizer = new StringTokenizer(in.readLine());
+            n = Integer.parseInt(tokenizer.nextToken());
+            m = Integer.parseInt(tokenizer.nextToken());
+            array = new int[m+1];
+            recursive(1);
+            out.write(builder.toString());
+            out.flush();
+        }
+    }
 
     public static void main(String[] args) {
         try {
-            P15650_3.execute();
+            P15651_3.execute();
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
