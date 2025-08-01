b597b4545bff57c655930a0b8677c6fc6fc4367a
edwardlee <openmoresome@gmail.com>
2025-08-01 20:00:12 +0900

    chapter 20 version 3 (5/8)

--- bj/src/Main.java
+++ bj/src/Main.java
@@ -130,10 +130,52 @@ public class Main {
             out.flush();
         }
     }
+    static class P9663_3 {
+        private static int n, numberOfCases = 0;
+        private static int[] board;
+
+        private static void putQueen(int row, int column) {
+            board[row] = column;
+        }
+        private static void removeQueen(int row, int column) {
+            board[row] = 0;
+        }
+        private static boolean isValid(int row, int column) {
+            for (int i = 1; i < row; i++) {
+                if (board[i] == column || Math.abs(i - row) == Math.abs(board[i] - column)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private static void recursive(int row) {
+            if (row > n) {
+                numberOfCases++;
+                return;
+            }
+
+            for (int col = 1; col <= n; col++) {
+                if (isValid(row, col)) {
+                    putQueen(row, col);
+                    recursive(row + 1);
+                    removeQueen(row, col);
+                }
+            }
+        }
+
+        public static void execute() throws IOException {
+            n = Integer.parseInt(in.readLine());
+            board = new int[n + 1];
+            recursive(1);
+            builder.append(numberOfCases);
+            out.write(builder.toString());
+            out.flush();
+        }
+    }
 
     public static void main(String[] args) {
         try {
-            P15652_3.execute();
+            P9663_3.execute();
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
