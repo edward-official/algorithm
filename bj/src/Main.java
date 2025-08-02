0853cb3962f0e5a5ec7b2b28c366f3ac493906f2
edwardlee <openmoresome@gmail.com>
2025-08-02 10:12:15 +0900

    chapter 20 version 3 (6/8)

--- bj/src/Main.java
+++ bj/src/Main.java
@@ -163,10 +163,78 @@ public class Main {
             out.flush();
         }
     }
+    static class P2580_3 {
+        private static int length = 9;
+        private static int[][] board;
+        private static int[] checkRows;
+        private static int[] checkColumns;
+        private static int numberOfCheckPoints = 0;
+        private static boolean didFound = false;
+
+        private static void boardOnTerminal() throws IOException {
+            for(int row=1; row<=length; row++) {
+                builder.append(board[row][1]);
+                for(int column=2; column<=length; column++) {
+                    builder.append(" ").append(board[row][column]);
+                }
+                builder.append("\n");
+            }
+            out.write(builder.toString());
+            out.flush();
+        }
+        private static boolean isPossible(int element, int row, int column) {
+            for(int traverse=1; traverse<=length; traverse++) {
+                if(board[traverse][column]==element) return false;
+                else if(board[row][traverse]==element) return false;
+            }
+            int closingRow = ((row-1)/3+1)*3;
+            int closingColumn = ((column-1)/3+1)*3;
+            for(int r=closingRow-2; r<=closingRow; r++) {
+                for(int c=closingColumn-2; c<=closingColumn; c++) {
+                    if(board[r][c]==element) return false;
+                }
+            }
+            return true;
+        }
+        private static void recursive(int index) throws IOException {
+            if(didFound) return;
+            if(index>numberOfCheckPoints) {
+                didFound = true;
+                boardOnTerminal();
+                return;
+            }
+            for(int element=1; element<=length; element++) {
+                if(isPossible(element,checkRows[index],checkColumns[index])) {
+                    board[checkRows[index]][checkColumns[index]] = element;
+                    recursive(index+1);
+                    board[checkRows[index]][checkColumns[index]] = 0;
+                }
+            }
+        }
+        public static void execute() throws IOException {
+            board = new int[length+1][length+1];
+            checkRows = new int[length*length+1];
+            checkColumns = new int[length*length+1];
+
+            for(int row=1; row<=length; row++) {
+                tokenizer = new StringTokenizer(in.readLine());
+                for(int column=1; column<=length; column++) {
+                    board[row][column] = Integer.parseInt(tokenizer.nextToken());
+                    if(board[row][column]==0) {
+                        numberOfCheckPoints++;
+                        checkRows[numberOfCheckPoints] = row;
+                        checkColumns[numberOfCheckPoints] = column;
+                    }
+                }
+            }
+
+            recursive(1);
+        }
+    }
 
     public static void main(String[] args) {
         try {
-            P9663_3.execute();
+            P2580_3.execute();
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
